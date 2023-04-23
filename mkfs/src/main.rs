f26f9bfb97c1ceb16640c1b5d1bc629d8ccc4a08
Hayden50 <haydenrieder50@hotmail.com>
2023-04-23 15:33:09 -0400

    Added writing inodes

--- mkfs/src/main.rs
+++ mkfs/src/main.rs
@@ -1,30 +1,12 @@
 // This is a standalone utility that creates the second disk structure (fs.img) when the makefile
 // is run. It runs on the host OS and defines the disk structure of the file system.
 
-use shared_fs::Superblock;
+use shared_fs::*;
 use endian_codec::{PackedSize, EncodeLE};
-// use shared_fs::{Inode, InodeType, Superblock};
 use std::env;
 use std::fs::File;
 use std::io::{Result, Seek, SeekFrom, Write};
 use std::path::Path;
-
-const FS_IMG: &str = "fs.img";
-const BSIZE: u32 = 512; // Size of one block
-const FSSIZE: u32 = 1000; // Number of blocks in FS
-const LOGSIZE: u32 = 30; // Number of blocks dedicated to log
-
-// const NDIR: u32 = 12;
-// const NINDIRECT: u32 = BSIZE as u32 / 4;
-// const MAXFILE: u32 = NDIR + NINDIRECT;
-
-// const ROOT_INO: u32 = 1; // Root Inode number
-// const IBLOCK: u32 = 2;
-// const BBLOCK: u32 = 3;
-
-const NINODES: u32 = 200; // Number of Inodes
-const IPB: u32 = BSIZE as u32 / 64; // Inodes per block
-const NINODEBLOCKS: u32 = NINODES / IPB; // Number of blocks holding Inodes
 
 fn main() -> Result<()> {
     // Gathers args into a Vec and makes sure the minimum is met
@@ -35,33 +17,20 @@ fn main() -> Result<()> {
     }
 
     // Create the fs.img file
-    let fs_img_path = Path::new(FS_IMG);
+    let fs_img_path = Path::new("fs.img");
     let mut fs_img = File::create(&fs_img_path)?;
 
     // Initialize fs.img with zero bytes
     fs_img.set_len((BSIZE * FSSIZE) as u64)?;
     zero_fs(&mut fs_img).unwrap();
-
-    let nlog = LOGSIZE;
-    let nbitmap = FSSIZE / (BSIZE * 8) + 1;
-    let nmeta = 2 + nlog + NINODEBLOCKS + nbitmap;
-    let nblocks = FSSIZE - nmeta;
-
+    
     // Define values in the superblock struct
-    let superblock = Superblock {
-        size: FSSIZE as u32,
-        nblocks,
-        ninodes: NINODES,
-        nlog: LOGSIZE,
-        logstart: 2,
-        inodestart: 2 + LOGSIZE,
-        bmapstart: 2 + LOGSIZE + NINODEBLOCKS,
-    };
+    let superblock = Superblock::new();
     write_superblock(&mut fs_img, &superblock)?;
 
     // Initialize the root directory
-    // let root_inode = Inode::new(InodeType::Dir);
-    // write_inode(&mut fs_img, ROOT_INO, &root_inode)?;
+    let root_inode = Inode::new(InodeType::Dir);
+    write_inode(&mut fs_img, ROOT_INO, &root_inode, &superblock)?;
 
     // Add any additional files, directories, or user programs to the file system
     // ...
@@ -96,24 +65,20 @@ fn write_superblock(fs_img: &mut File, superblock: &Superblock) -> Result<()> {
     Ok(())
 }
 
+// Writes an inode to disk at a specified inum
+fn write_inode(fs_img: &mut File, inum: u32, inode: &Inode, superblock: &Superblock) -> Result<()> {
+    let inode_block = (inum / IPB) + superblock.inodestart;
+    let inode_offset = (inum % IPB) * INODE_SIZE;
+    
+    fs_img.seek(SeekFrom::Start((inode_block * BSIZE + inode_offset) as u64))?;
+
+    let mut inode_encoded = [0; Inode::PACKED_LEN];
+    inode.encode_as_le_bytes(&mut inode_encoded);
+    
+    fs_img.write(&inode_encoded)?;
+
+    Ok(())
+}
+ 
 // fn ialloc(i_type: InodeType) {}
 // fn balloc() {}
-
-// Writes an inode to disk at a specified inum
-// fn write_inode(fs_img: &mut File, inum: u32, inode: &Inode) -> Result<()> {
-    // fs_img.seek(SeekFrom::Start(
-    //     BSIZE as u64 * (IBLOCK as u64 + inum as u64 / IPB as u64),
-    // ))?;
-    // fs_img.write_u8(inode.inode_type as u8)?;
-    // fs_img.write_u8(inode.major_dev)?;
-    // fs_img.write_u8(inode.minor_dev)?;
-    // fs_img.write_u8(inode.num_links)?;
-    // fs_img.write_u32::<LittleEndian>(inode.size)?;
-    // for &address in inode.data_addreses.iter() {
-    //     fs_img.write_u32::<LittleEndian>(address)?;
-    // }
-//     Ok(())
-// }
-
-// Implement additional helper functions to handle file system operations
-// like creating files, directories, writing data blocks, etc.
--- shared_fs/src/lib.rs
+++ shared_fs/src/lib.rs
@@ -1,9 +1,27 @@
 #![no_std]
 use endian_codec::{PackedSize, EncodeLE, DecodeLE};
+use core::mem;
 
-pub const ROOTINO: u32 = 1;  // root i-number
-pub const BSIZE: u32 = 512;  // block size
-pub const NDIRECT: u32 = 12;
+pub const BSIZE: u32 = 512;  // Block size pub const
+pub const NDIRECT: u32 = 12; // Number of directly referenced blocks for each Inode
+pub const FSSIZE: u32 = 1000; // Number of blocks in FS
+pub const LOGSIZE: u32 = 10; // Number of blocks dedicated to log
+
+// const NINDIRECT: u32 = BSIZE as u32 / 4;
+// const MAXFILE: u32 = NDIR + NINDIRECT;
+
+pub const ROOT_INO: u32 = 1; // Root Inode number
+// const IBLOCK: u32 = 2;
+// const BBLOCK: u32 = 3;
+
+pub const NINODES: u32 = 200; // Number of Inodes
+pub const INODE_SIZE: u32 = 60; // Size of each Inode in bytes
+pub const IPB: u32 = BSIZE as u32 / INODE_SIZE; // Inodes per block
+pub const NINODEBLOCKS: u32 = NINODES / IPB; // Number of blocks holding Inodes
+ 
+pub const NBITMAP: u32 = FSSIZE / (BSIZE * 8) + 1;
+pub const NMETA: u32 = 1 + LOGSIZE + NINODEBLOCKS + NBITMAP;
+pub const NBLOCKS: u32 = FSSIZE - NMETA;
  
 #[derive(Debug, PartialEq, EncodeLE, DecodeLE, PackedSize)]
 pub struct Superblock {
@@ -16,14 +34,14 @@ pub struct Superblock {
     pub bmapstart: u32,     // Block number of first free map block
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, EncodeLE, DecodeLE, PackedSize)]
 pub struct Inode {
-    pub inode_type: InodeType,                      // Represents type of file the Inode references
-    pub major_dev: u8,                              // Major device number
-    pub minor_dev: u8,                              // Minor device number
-    pub num_links: u8,                              // Number of symbolic links associated with this file 
-    pub size: u32,                                  // Total number of bytes of content of a file
-    pub data_addreses: [u32; NDIRECT as usize + 1], // Records the block numbers of the disk 
+    pub inode_type: InodeType,          // Represents type of file the Inode references
+    pub major_dev: u8,                  // Major device number
+    pub minor_dev: u8,                  // Minor device number
+    pub num_links: u8,                  // Number of symbolic links associated with this file 
+    pub size: u32,                      // Total number of bytes of content of a file
+    pub data_addreses: DataAddresses,   // Records the block numbers of the disk
 }
 
 pub struct Dirent {
@@ -40,6 +58,58 @@ pub enum InodeType {
     Device = 3,
 }
 
+// Wrapper for u32 array for serialization / deserialization purposes
+#[derive(Debug, PartialEq)]
+pub struct DataAddresses([u32; 13]);
+
+impl PackedSize for DataAddresses {
+   const PACKED_LEN: usize = 13 * 4; 
+}
+
+impl EncodeLE for DataAddresses {
+    fn encode_as_le_bytes(&self, bytes: &mut [u8]) {
+        let mut idx = 0;
+        for value in &self.0 {
+            value.encode_as_le_bytes(&mut bytes[idx..idx + mem::size_of::<u32>()]);
+            idx += mem::size_of::<u32>();
+        }
+    }
+}
+
+impl DecodeLE for DataAddresses {
+    fn decode_from_le_bytes(bytes: &[u8]) -> Self {
+        let mut data_addresses = [0u32; 13];
+        let mut idx = 0;
+        for value in data_addresses.iter_mut() {
+            *value = u32::decode_from_le_bytes(&bytes[idx..idx + mem::size_of::<u32>()]);
+            idx += mem::size_of::<u32>();
+        }
+        DataAddresses(data_addresses)
+    }
+}
+
+impl PackedSize for InodeType {
+    const PACKED_LEN: usize = 1;
+}
+
+impl EncodeLE for InodeType {
+    fn encode_as_le_bytes(&self, bytes: &mut[u8]) {
+        bytes[0] = *self as u8;
+    }
+}
+
+impl DecodeLE for InodeType {
+    fn decode_from_le_bytes(bytes: &[u8]) -> Self {
+        let value = bytes[0];
+        match value {
+            1 => Self::Dir,
+            2 => Self::File,
+            3 => Self::Device,
+            _ => Self::Free,
+        }
+    }
+}
+
 impl Inode {
     pub fn new(inode_type: InodeType) -> Self {
         Inode {
@@ -48,7 +118,21 @@ impl Inode {
             minor_dev: 0,
             num_links: 1,
             size: 0,
-            data_addreses: [0; NDIRECT as usize + 1],
+            data_addreses: DataAddresses([0; NDIRECT as usize + 1]),
+        }
+    }
+}
+
+impl Superblock {
+    pub fn new() -> Self {
+        Superblock {
+            size: FSSIZE as u32,
+            nblocks: NBLOCKS,
+            ninodes: NINODES,
+            nlog: LOGSIZE,
+            logstart: 1,
+            inodestart: 1 + LOGSIZE,
+            bmapstart: 1 + LOGSIZE + NINODEBLOCKS,
         }
     }
 }
