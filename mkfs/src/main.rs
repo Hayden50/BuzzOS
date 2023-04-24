07018b303001bbc0160b4c3a42faf74a38e7a190
Hayden50 <haydenrieder50@hotmail.com>
2023-04-23 22:40:22 -0400

    Add ialloc, balloc, read_inode, and updated Superblock

--- mkfs/src/main.rs
+++ mkfs/src/main.rs
@@ -2,10 +2,10 @@
 // is run. It runs on the host OS and defines the disk structure of the file system.
 
 use shared_fs::*;
-use endian_codec::{PackedSize, EncodeLE};
+use endian_codec::{PackedSize, EncodeLE, DecodeLE};
 use std::env;
-use std::fs::File;
-use std::io::{Result, Seek, SeekFrom, Write};
+use std::fs::{OpenOptions, File};
+use std::io::{Read, Result, Seek, SeekFrom, Write};
 use std::path::Path;
 
 fn main() -> Result<()> {
@@ -18,7 +18,11 @@ fn main() -> Result<()> {
 
     // Create the fs.img file
     let fs_img_path = Path::new("fs.img");
-    let mut fs_img = File::create(&fs_img_path)?;
+    let mut fs_img = OpenOptions::new()
+        .create(true)
+        .read(true)
+        .write(true)
+        .open(&fs_img_path)?;
 
     // Initialize fs.img with all bytes written to zero
     fs_img.set_len((BSIZE * FSSIZE) as u64)?;
@@ -30,7 +34,7 @@ fn main() -> Result<()> {
 
     // Initialize the root directory by creating an Inode / allocating data blocks
     let mut root_inode = Inode::new(InodeType::Dir);
-    let root_data_block = root_balloc(&mut fs_img);
+    let root_data_block = balloc(&mut fs_img, &superblock);
     root_inode.data_addresses.0[0] = root_data_block.unwrap();
     write_inode(&mut fs_img, ROOT_INO, &root_inode, &superblock)?;
     
@@ -41,7 +45,6 @@ fn main() -> Result<()> {
     // Write the "." and ".." directory entries to the root directory's data block
     write_dirent(&mut fs_img, &dot_entry, root_inode.data_addresses.0[0], 0)?;
     write_dirent(&mut fs_img, &dotdot_entry, root_inode.data_addresses.0[0], 1)?;
-    
 
     // Add any additional files, directories, or user programs to the file system
     // ...
@@ -91,22 +94,12 @@ fn write_inode(fs_img: &mut File, inum: u32, inode: &Inode, superblock: &Superbl
     Ok(())
 }
 
-// Allocates the block for the root Inode and returns the block number (avoids searching bitmap
-// because nothing has been written)
-fn root_balloc(fs_img: &mut File) -> Result<u32> {
-    fs_img.seek(SeekFrom::Start(((1 + LOGSIZE + NINODEBLOCKS) * BSIZE) as u64))?;
-    let mut onebit: [u8; 1] = [0; 1];
-    onebit[0] |= 1 << 7;
-    fs_img.write(&onebit)?;
-    Ok(NMETA)
-}
-
 // Writes a directory entry to a specified data block and index in the file system
 fn write_dirent(fs_img: &mut File, dirent: &Dirent, block: u32, index: usize) -> Result<()> {
     let dirent_start = block * BSIZE + (index * std::mem::size_of::<Dirent>()) as u32;
     fs_img.seek(SeekFrom::Start(dirent_start as u64))?;
 
-    let mut dirent_encoded = [0; std::mem::size_of::<Dirent>()];
+    let mut dirent_encoded = [0; Dirent::PACKED_LEN];
     dirent.encode_as_le_bytes(&mut dirent_encoded);
 
     fs_img.write(&dirent_encoded)?;
@@ -114,4 +107,73 @@ fn write_dirent(fs_img: &mut File, dirent: &Dirent, block: u32, index: usize) ->
     Ok(())
 }
 
-// fn ialloc(i_type: InodeType) {}
+// Allocate an i number
+fn ialloc(fs_img: &mut File, superblock: &Superblock) -> Result<u32> {
+    for inum in 1..superblock.ninodes {
+        // Read the inode from the disk
+        let inode = read_inode(fs_img, inum, superblock)?;
+
+        // Check if the inode is unused
+        if let InodeType::Free = inode.inode_type {
+            return Ok(inum);
+        }
+    }
+
+    // If no unused inodes are found, return an error
+    Err(std::io::Error::new(
+        std::io::ErrorKind::Other,
+        "No unused inodes available",
+    ))
+}
+
+// Helper method for ialloc to check if the current inode is free or not
+fn read_inode(fs_img: &mut File, inum: u32, superblock: &Superblock) -> Result<Inode> {
+    let inode_block = (inum / IPB) + superblock.inodestart;
+    let inode_offset = (inum % IPB) * INODE_SIZE;
+
+    fs_img.seek(SeekFrom::Start((inode_block * BSIZE + inode_offset) as u64))?;
+
+    let mut inode_encoded: [u8; Inode::PACKED_LEN] = [0; Inode::PACKED_LEN];
+    fs_img.read_exact(&mut inode_encoded)?;
+
+    let inode = Inode::decode_from_le_bytes(&inode_encoded);
+
+    Ok(inode)
+}
+
+// Checks bitmap for an available block and then allocates it
+fn balloc(fs_img: &mut File, superblock: &Superblock) -> Result<u32> {
+    let bitmap_start = superblock.bmapstart * BSIZE;
+    let mut bitmap = vec![0; BSIZE as usize];
+    fs_img.seek(SeekFrom::Start(bitmap_start as u64))?;
+    fs_img.read_exact(&mut bitmap)?;
+
+    let data_start_block = superblock.datastart;
+
+    let mut free_block: Option<u32> = None;
+    for (i, byte) in bitmap.iter().enumerate() {
+        for j in 0..8 {
+            if byte & (1 << j) == 0 {
+                free_block = Some((i * 8 + j) as u32 + data_start_block);
+                break;
+            }
+        }
+        if free_block.is_some() {
+            break;
+        }
+    }
+
+    let free_block = free_block.ok_or_else(|| std::io::Error::new(std::io::ErrorKind::Other, "No free blocks available"))?;
+
+    let index = (free_block - data_start_block) / 8;
+    let bit = (free_block - data_start_block) % 8;
+    bitmap[index as usize] |= 1 << bit;
+
+    fs_img.seek(SeekFrom::Start(bitmap_start as u64))?;
+    fs_img.write_all(&bitmap)?;
+
+    fs_img.seek(SeekFrom::Start((free_block * BSIZE) as u64))?;
+    fs_img.write_all(&vec![0; BSIZE as usize])?;
+
+    Ok(free_block)
+}
--- shared_fs/src/superblock.rs
+++ shared_fs/src/superblock.rs
@@ -1,4 +1,4 @@
-use crate::{FSSIZE, NBLOCKS, NINODES, LOGSIZE, NINODEBLOCKS};
+use crate::{BSIZE, FSSIZE, NBLOCKS, NINODES, LOGSIZE, NINODEBLOCKS};
 use endian_codec::{PackedSize, EncodeLE, DecodeLE};
 
 #[derive(Debug, PartialEq, EncodeLE, DecodeLE, PackedSize)]
@@ -10,18 +10,26 @@ pub struct Superblock {
     pub logstart: u32,      // Block number of first log block
     pub inodestart: u32,    // Block number of first inode block
     pub bmapstart: u32,     // Block number of first free map block
+    pub datastart: u32,
 }
 
 impl Superblock {
     pub fn new() -> Self {
+        let logstart = 1;
+        let inodestart = logstart + LOGSIZE;
+        let bmapstart = inodestart + NINODEBLOCKS;
+        let bitmap_blocks = NBLOCKS / (8 * BSIZE as u32) + ((NBLOCKS % (8 * BSIZE as u32)) != 0) as u32;
+        let datastart = bmapstart + bitmap_blocks;
+
         Superblock {
             size: FSSIZE as u32,
             nblocks: NBLOCKS,
             ninodes: NINODES,
             nlog: LOGSIZE,
-            logstart: 1,
-            inodestart: 1 + LOGSIZE,
-            bmapstart: 1 + LOGSIZE + NINODEBLOCKS,
+            logstart,
+            inodestart,
+            bmapstart,
+            datastart,
         }
     }
 }
